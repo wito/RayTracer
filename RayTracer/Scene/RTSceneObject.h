69badadf77f6f0eaa611b83c1fd0ed0fb417cf01
Williham Totland <wito@eyego.no>
2011-11-29 10:40:14 +0100

    Scale, translate, rotate + bounds

--- RayTracer/Scene/RTSceneObject.h
+++ RayTracer/Scene/RTSceneObject.h
@@ -7,7 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RTGeometry.h"
 
 @interface RTSceneObject : NSObject
+
+@property (assign) RTVector scale;
+@property (assign) RTVector translate;
+@property (assign) RTVector rotate;
+
+- (void)scaleBy:(RTVector)v;
+- (void)translateBy:(RTVector)v;
+- (void)rotateBy:(RTVector)v;
+
+@property (readonly,assign) RTBounds bounds;
 
 @end
--- RayTracer/Scene/RTSceneObject.m
+++ RayTracer/Scene/RTSceneObject.m
@@ -10,4 +10,34 @@
 
 @implementation RTSceneObject
 
+@synthesize scale = _scale;
+@synthesize translate = _translate;
+@synthesize rotate = _rotate;
+
+@dynamic bounds;
+
+- (id)init {
+  self = [super init];
+
+  if (self) {
+    _scale = RTMakeVector(1.0, 1.0, 1.0);
+    _translate = RTMakeVector(0.0, 0.0, 0.0);
+    _rotate = RTMakeVector(0.0, 0.0, 0.0);
+  }
+  
+  return self;
+}
+
+- (void)scaleBy:(RTVector)v {
+  self.scale = RTVectorAddition(self.scale, v);
+}
+
+- (void)translateBy:(RTVector)v {
+  self.translate = RTVectorAddition(self.translate, v);
+}
+
+- (void)rotateBy:(RTVector)v {
+  self.rotate = RTVectorAddition(self.rotate, v);
+}
+
 @end
