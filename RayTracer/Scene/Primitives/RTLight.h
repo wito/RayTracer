152934df9eb3c812257966792c01828176caafe9
Williham Totland <wito@eyego.no>
2011-11-30 13:29:38 +0100

    RTLight

---
+++ RayTracer/Scene/Primitives/RTLight.h
@@ -0,0 +1,15 @@
+//
+//  RTLight.h
+//  RayTracer
+//
+//  Created by Williham Totland on 20111130.
+//  Copyright (c) 2011 Eyego. All rights reserved.
+//
+
+#import "RTPrimitive.h"
+
+@interface RTLight : RTPrimitive
+
+@property (assign) CGFloat intensity;
+
+@end
---
+++ RayTracer/Scene/Primitives/RTLight.m
@@ -0,0 +1,15 @@
+//
+//  RTLight.m
+//  RayTracer
+//
+//  Created by Williham Totland on 20111130.
+//  Copyright (c) 2011 Eyego. All rights reserved.
+//
+
+#import "RTLight.h"
+
+@implementation RTLight
+
+@synthesize intensity;
+
+@end
